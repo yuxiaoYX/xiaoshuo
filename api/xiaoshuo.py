6ea1d9a81222f07ff41b3e3c01692448d1dc87cb
yuxiaoYX <2923557334@qq.com>
2019-06-25 18:08:48 +0800

    列表转字典的问题还在解决中

--- api/xiaoshuo.py
+++ api/xiaoshuo.py
@@ -20,17 +20,50 @@ def parse_url(**kw):
 
 
 def book_search(**kw):
-    print(kw)
+    sources_data={
+        'bookName':'//*[@id="nr"]/td[1]/a/text()',
+        'bookAuthor':'//*[@id="nr"]/td[3]/text()',
+        'bookNewestChapterName':'//*[@id="nr"]/td[5]/text()',
+        'bookUpdateTime':'//*[@id="nr"]/td[2]/a/text()',
+        'bookUrl':'//*[@id="nr"]/td[1]/a/@href'
+    }
 
+    print(kw)
     search_data = parse_url(**kw)
     # print(search_data.text)
     html = etree.HTML(search_data.text)
-    html_data=html.xpath('//*[@id="conn"]/table/tbody')
-    print(html_data)
-    print(html.tostring(html_data[0]))
-    # aa=html_data.xpath('@td')
-    # print(aa)
+    # b='//*[@id="nr"]/td/a/text() | //*[@id="nr"]/td/a/@href | //*[@id="nr"]/td/text()'
+    c = ['book_name','book_author','book_newest_hapter_ame','book_update_time','book_url']
+    for k,v in sources_data.items():
+        cc=html.xpath(v)
+        print({k:cc})
+    
+
+    # c=['bookName','bookAuthor','bookNewestChapterName','bookUpdateTime','bookUrl']
+    # cc=['https://www.i7wx.com/book/35344.html', '剑来', ' 第653章 可惜下雨不下钱（二）', '烽火戏诸侯', '19-05-21', 'https://www.i7wx.com/book/75523.html', '剑来', ' 第九章：痴……女？', '老油条本尊',  '19-01-21',  'https://www.i7wx.com/book/4473.html', '剑来',  ' 第四十五章 白马下青山', '暮鼓晨钟', '18-03-03']
+    # ccc=(cc[i:i+5] for i in range(0,len(cc),5))
+    # cccc=[dict(zip(c,ii)) for ii in ccc]
+    # print(cccc)
 
 if __name__ == '__main__':
     pass
-    book_search(url='https://www.i7wx.com/modules/article/search.php?searchkey=%BD%A3%C0%B4&searchtype=articlename',method='get',data='',charset='gbk')
+    # book_search(url='https://www.i7wx.com/modules/article/search.php?searchkey=%BD%A3%C0%B4&searchtype=articlename',method='get',data='',charset='gbk')
+    # book_search(url='http://www.88xiaoshuo.com/modules/article/search.php?searchkey=%BD%A3%C0%B4&searchtype=articlename',method='get',data='',charset='gbk')
+
+
+
+
+a=[['剑来1', '剑来2', '剑来3'], ['烽火戏诸侯', '老油条本尊', '暮鼓晨钟']]
+b=['bookName','bookAuthor']
+
+c=[
+    {'bookName':'剑来1','bookAuthor':'烽火戏诸侯'},
+    {'bookName':'剑来2','bookAuthor':'老油条本尊'},
+    {'bookName':'剑来3','bookAuthor':'暮鼓晨钟'},
+]
+
+for i in a:
+    print(a)
+    for ii in i :
+        print(i)
+
